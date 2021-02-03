f3fff443f7d8270574ad9c1476feb73f5e4e5ed2
Gusted <williamzijl7@hotmail.com>
2021-02-03 19:43:43 +0100

    Use ESBuild's watch
    - As of ESBuild 0.8.38, ESBuild has native(go implementation) support for watching files.
    - Removes chokidar.
    - Upped peer + dev dependency to 0.8.38

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import chokidar, { FSWatcher } from "chokidar";
 import { debounce, formatTime } from "./utils";
 import * as karma from "karma";
 import * as esbuild from "esbuild";
@@ -66,6 +65,7 @@ function createPreprocessor(
 		const result = await service!.build({
 			target: "es2015",
 			...userConfig,
+			watch,
 			bundle: true,
 			write: false,
 			entryPoints: files,
@@ -85,29 +85,21 @@ function createPreprocessor(
 
 	const entries = new Set<string>();
 
-	let watcher: FSWatcher | null = null;
 	const watchMode = !config.singleRun && !!config.autoWatch;
-	if (watchMode) {
-		// Initialize watcher to listen for changes in basePath so
-		// that we'll be notified of any new files
-		const basePath = config.basePath || process.cwd();
-		watcher = chokidar.watch([basePath], {
-			ignoreInitial: true,
-			// Ignore dot files and anything from node_modules
-			ignored: /((^|[/\\])\..|node_modules)/,
-		});
-		// Register shutdown handler
-		emitter.on("exit", done => {
-			watcher!.close();
-			done();
-		});
-
-		const onWatch = debounce(async () => {
-			emitter.refreshFiles();
-		}, 100);
-		watcher.on("change", onWatch);
-		watcher.on("add", onWatch);
-	}
+	const onWatchChange = debounce(async () => {
+		emitter.refreshFiles();
+	}, 100);
+	const watch = watchMode
+		? {
+				onRebuild(
+					error: esbuild.BuildFailure | null,
+					result: esbuild.BuildResult | null,
+				) {
+					error && log.error(error.message);
+					result && onWatchChange();
+				},
+		  }
+		: false;
 
 	const afterPreprocess = (time: number) => {
 		logDone(Date.now() - time);
