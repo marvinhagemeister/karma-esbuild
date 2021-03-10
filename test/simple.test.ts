5dc31f87d754886752bc37d0796feab4fe9199f0
Justin Ridgewell <justin@ridgewell.name>
2021-03-10 02:58:49 -0500

    Rename simple test fixture's directory
    It was really bugging me that it was the only one that went `yarn run-karma simple-test`, while all the others excluded `-test`.

--- test/simple.test.ts
+++ test/simple.test.ts
@@ -3,7 +3,7 @@ import { assertEventuallyProgresses, runKarma } from "./test-utils";
 
 export const description = "Run a single test";
 export async function run(config: Config) {
-	const { output } = await runKarma(config, "simple-test");
+	const { output } = await runKarma(config, "simple");
 
 	await assertEventuallyProgresses(output.stdout, () => {
 		return output.stdout.some(line => /1 test completed/.test(line));
