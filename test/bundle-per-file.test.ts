5f8945f2ca1c3af3b2429d5abfecb089c4740355
Marvin Hagemeister <hello@marvinh.dev>
2021-05-14 18:31:49 +0200

    Switch bundle-per-file test to use global state

--- test/bundle-per-file.test.ts
+++ test/bundle-per-file.test.ts
@@ -1,6 +1,4 @@
 import { Config } from "pentf/config";
-import { strict as assert } from "assert";
-import fetch from "node-fetch";
 import { assertEventuallyProgresses, runKarma } from "./test-utils";
 
 export const description = "Create a separate bundle per test file";
@@ -12,31 +10,4 @@ export async function run(config: Config) {
 	await assertEventuallyProgresses(output.stdout, () => {
 		return output.stdout.some(line => /2 tests completed/.test(line));
 	});
-
-	let address;
-	for (const line of output.stdout) {
-		const match = line.match(/(https?:\/\/localhost:\d+)/);
-		if (match) {
-			address = match[1];
-		}
-	}
-
-	// Check that dependency is not inlined
-	let res = await fetch(`${address}/base/files/main-a.js`);
-	let text = await res.text();
-	assert.match(text, /CONTENT/);
-
-	res = await fetch(`${address}/base/files/main-b.js`);
-	text = await res.text();
-	assert.match(text, /CONTENT/);
-
-	// Check that we have no duplicate "compiling..." messages
-	assert.equal(
-		output.stdout.filter(line => /Compiling\.\.\./i.test(line)).length,
-		1,
-	);
-	assert.equal(
-		output.stdout.filter(line => /Compiling done/i.test(line)).length,
-		1,
-	);
 }
