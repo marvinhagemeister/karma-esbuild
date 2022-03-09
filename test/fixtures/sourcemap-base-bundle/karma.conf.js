290e00fdcb9fa6d36172eb55e7ae2a2322bdfd60
Marvin Hagemeister <hello@marvinh.dev>
2022-03-10 00:34:52 +0100

    Simplify base source map test case

--- test/fixtures/sourcemap-base-bundle/karma.conf.js
+++ test/fixtures/sourcemap-base-bundle/karma.conf.js
@@ -1,37 +1,4 @@
 const { baseConfig } = require("../../base.karma.conf");
-const path = require("path");
-
-const expectedSources = [
-	path.join(process.cwd(), "test", "fetch-polyfill.js"),
-	path.join(
-		process.cwd(),
-		"test",
-		"fixtures",
-		"sourcemap-base-bundle",
-		"files",
-		"main-a.js",
-	),
-].sort();
-
-const envPlugin = {
-	name: "env",
-	setup(build) {
-		// Intercept import paths called "env" so esbuild doesn't attempt
-		// to map them to a file system location. Tag them with the "env-ns"
-		// namespace to reserve them for this plugin.
-		build.onResolve({ filter: /^env$/ }, args => ({
-			path: args.path,
-			namespace: "env-ns",
-		}));
-
-		// We're going to hook into esbuild and replace the "env" module loaded
-		// by the tests with our expected results.
-		build.onLoad({ filter: /^env$/, namespace: "env-ns" }, () => ({
-			contents: JSON.stringify(expectedSources),
-			loader: "json",
-		}));
-	},
-};
 
 module.exports = function (config) {
 	config.set({
@@ -43,7 +10,6 @@ module.exports = function (config) {
 
 		esbuild: {
 			singleBundle: false,
-			plugins: [envPlugin],
 		},
 	});
 };
--- test/sourcemap-base-bundle.test.ts
+++ test/sourcemap-base-bundle.test.ts
@@ -7,11 +7,18 @@ export async function run(config: any) {
 	const { output } = await runKarma(config, "sourcemap-base-bundle");
 
 	await assertEventuallyProgresses(output.stdout, () => {
-		return output.stdout.some(line => /1 test completed/.test(line));
+		return output.stdout.some(line => /SUMMARY/.test(line));
 	});
 
-	// Check for mapped file
-	const reg = /at.*\((.*):\d+:\d+\)/;
-	const trace = output.stdout.find(line => reg.test(line)) || "";
-	assert.match(trace, /dep2\.js:3:1/);
+	// Check for mapped source file
+	await assertEventuallyProgresses(output.stdout, () => {
+		assert.match(output.stdout.join("\n"), /main-a\.js:3:9/);
+		return true;
+	});
+
+	// Check for mapped dependency file
+	await assertEventuallyProgresses(output.stdout, () => {
+		assert.match(output.stdout.join("\n"), /dep2\.js:3:1/);
+		return true;
+	});
 }
